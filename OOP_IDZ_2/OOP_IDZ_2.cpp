a4585f0319a4585f00308e5924a6df5c30805972
ndrlptn <105172985+ndrlptn@users.noreply.github.com>
2023-10-23 22:10:00 +1000

    Add files via upload

---
+++ OOP_IDZ_2/OOP_IDZ_2.cpp
@@ -0,0 +1,89 @@
+﻿#include <iostream>
+#include <limits>
+
+
+const int N = 3, M = 1;
+
+
+void sort(double** matrix);
+void makeMatrix(double** &matrix);
+void printMatrix(double** matrix);
+
+
+int main()
+{
+	double** matrix = new double*[N];
+	
+	makeMatrix(matrix);
+	printMatrix(matrix);
+
+	sort(matrix);
+	printMatrix(matrix);
+
+}
+
+
+void makeMatrix(double** &matrix)
+{
+	double val;
+
+	for (int i = 0; i < N; ++i)
+	{
+		double* vector = new double[M];
+
+		for (int j = 0; j < M; ++j)
+		{
+			std::cout << "input value: ";
+			std::cin >> val;
+			*vector = val;
+			++vector;
+		}
+		*matrix = vector - M;
+		++matrix;
+	}
+	matrix = matrix - N;
+}
+
+void printMatrix(double** matrix)
+{
+	for (int i = 0; i < N; ++i)
+	{
+		for (int j = 0; j < M; ++j)
+		{
+			std::cout << **matrix << ' ';
+			++*matrix;
+		}
+		*matrix = *matrix - M;
+		++matrix;
+		std::cout << '\n';
+	}
+	std::cout << '\n';
+	matrix = matrix - N;
+}
+
+void sort(double** matrix)
+{
+	double** arrow = matrix;
+
+	for (int i = 0; i < N; ++i)
+	{
+		arrow = matrix + i;
+		for (int j = i + 1; j <= N - 1; ++j)
+		{
+			
+			double** arrow1 = matrix + j;
+
+			if (**arrow > **arrow1)
+			{
+				for (int k = 0; k < M; ++k)
+				{
+					double memory = **arrow;
+
+					*(*(matrix + i) + k) = *(*(matrix + j) + k);
+					*(*(matrix + j) + k) = memory;
+				}
+			}
+		}
+	}
+	
+}
