0c55867a7270c77a4118c4c54bd6b7438f8f65f6
ndrlptn <105172985+ndrlptn@users.noreply.github.com>
2023-10-23 22:09:03 +1000

    Add files via upload

---
+++ OOP_IDZ_1/OOP_IDZ_1.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <cmath>
+#include <limits>
+
+const double pi = 3.14159;
+
+
+double foo(double x, int n);
+
+
+int main()
+{
+	double x;
+	int n;
+	std::cout << "Input x: \n";
+	std::cin >> x;
+	while (std::cin.fail())
+	{
+		std::cout << "Input correct x: \n";
+		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		std::cin >> x;
+		
+	}
+	
+
+	std::cout << "input n: \n" << '\n';
+	std::cin >> n;
+	while (std::cin.fail())
+	{
+		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		std::cout << "Input correct n: \n";
+		std::cin >> n;
+	}
+	
+
+	double y = 0;
+
+	y = foo(x, n);
+
+	std::cout << y;
+	return 0;
+}
+
+
+double foo(double x, int n)
+{
+	double y = 0;
+	double m = 1; //multiplication result
+
+	y =  sin(pi);
+
+	for (int k = 5; k < n; ++k)
+	{
+		m = m * ( 1 + (x * std::log(k)) / ( (x + 1) / (1 + std::pow(k,2)) ) );
+	}
+
+	y = y + m;
+
+	return y;
+}
